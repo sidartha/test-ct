0efbce6d298882ce0ab8b9ae45fa9e0a5ffbe9af
yash-kantharia <57346148+yash-kantharia@users.noreply.github.com>
2019-11-20 17:20:23 +0530

    Update inappajaxform.js

--- inappajaxform.js
+++ inappajaxform.js
@@ -1,4 +1,4 @@
-var $form = $('form#test-form'),
+var $form = $('#test-form'),
     url = 'https://script.google.com/macros/s/AKfycbz2GvOjQNDiyNjgkzNT93RnlR2aMROaRqOgV3aq1rjO97BMGcWB/exec'
 
 $('#submit-form').on('click', function(e) {
@@ -9,6 +9,5 @@ $('#submit-form').on('click', function(e) {
     dataType: "json",
     data: $form.serializeObject()
   }).success(
-    console.log("Done");
   );
 })
