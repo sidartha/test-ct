7ba744a009c4b991c950e4434e64dc486c9844a7
yash-kantharia <57346148+yash-kantharia@users.noreply.github.com>
2019-11-20 17:06:57 +0530

    Update inappajaxform.js

--- inappajaxform.js
+++ inappajaxform.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+    url = 'https://script.google.com/macros/s/AKfycbz2GvOjQNDiyNjgkzNT93RnlR2aMROaRqOgV3aq1rjO97BMGcWB/exec'
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
